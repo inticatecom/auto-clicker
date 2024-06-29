a2c5368a3b00d222c23529b89cd7f94846677bbd
TwoBrake <Lucas_Stranks@outlook.com>
2024-06-29 00:04:42 -0700

    continue working on settings

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,7 +1,7 @@
-﻿using Inticate_Auto_Clicker.ViewModels;
+﻿using System.Diagnostics;
+using Inticate_Auto_Clicker.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using WinUIEx;
 
 namespace Inticate_Auto_Clicker.Views;
 
@@ -22,15 +22,14 @@ public sealed partial class SettingsPage : Page
         ViewModel = App.GetService<SettingsViewModel>();
         InitializeComponent();
 
-        // Restore settings.
-        NotificationsCheckBox.IsChecked = NOTIFICATIONS_ENABLED;
-        AlwaysOnTopCheckBox.IsChecked = ALWAYS_ON_TOP;
+        LoadSettings();
     }
 
     private void NotificationsToggled(object sender, RoutedEventArgs e)
     {
         var checkBox = (CheckBox)sender;
         NOTIFICATIONS_ENABLED = (bool)checkBox.IsChecked;
+        SaveSettings();
     }
 
     private void AlwaysOnTopToggled(object sender, RoutedEventArgs e)
@@ -38,5 +37,34 @@ public sealed partial class SettingsPage : Page
         var checkBox = (CheckBox)sender;
         ALWAYS_ON_TOP = (bool)checkBox.IsChecked;
         App.MainWindow.SetIsAlwaysOnTop(ALWAYS_ON_TOP);
+        SaveSettings();
+    }
+
+    private void SaveSettings()
+    {
+        var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+        localSettings.Values["NOTIFICATIONS_ENABLED"] = NOTIFICATIONS_ENABLED;
+        localSettings.Values["ALWAYS_ON_TOP"] = ALWAYS_ON_TOP;
+        Debug.WriteLine("Settings saved.");
+    }
+
+    private void LoadSettings()
+    {
+        var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+        if (localSettings.Values.TryGetValue("NOTIFICATIONS_ENABLED", out var notificationsEnabled))
+        {
+            NotificationsCheckBox.IsChecked = (bool)notificationsEnabled;
+            Debug.WriteLine("Notifications enabled: " + notificationsEnabled);
+        }
+
+        if (localSettings.Values.TryGetValue("ALWAYS_ON_TOP", out var alwaysOnTop))
+        {
+            AlwaysOnTopCheckBox.IsChecked = (bool)alwaysOnTop;
+            Debug.WriteLine("Always on top: " + alwaysOnTop);
+        }
+
+        Debug.WriteLine("Settings loaded");
     }
 }
